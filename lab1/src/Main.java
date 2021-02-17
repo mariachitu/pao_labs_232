a630e1ae5ec48a21abc440ca49ccdcc6b507fc67
Maria Cristina Chitu <Maria-Cristina.Chitu@endava.com>
2021-02-17 13:40:59 +0200

    Laborator 1

---
+++ lab1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public void hello()   {
+        //
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+// javac Main.java --> Main.class (bytecode) --> java Main --> execute ->> Hi
